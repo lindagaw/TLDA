c7d1b063e81e71bdff11397b46b661968dcf8919
Ashley Gao <gaoye0718@gmail.com>
2021-05-17 16:01:03 -0400

    wip

--- params.py
+++ params.py
@@ -31,7 +31,7 @@ d_model_restore = "snapshots/ADDA-critic-final.pt"
 
 # params for training network
 num_gpu = 1
-num_epochs_pre = 10
+num_epochs_pre = 6
 log_step_pre = 20
 eval_step_pre = 20
 save_step_pre = 100
