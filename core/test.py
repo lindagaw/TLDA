0843bdb4710c123aba58fdfdc10d52dd7adf86aa
Ashley Gao <gaoye0718@gmail.com>
2021-05-18 17:50:24 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -9,64 +9,66 @@ from utils import make_variable
 def eval_tgt(src_encoder, tgt_encoder, classifier, data_loader, src_detector, tgt_detector):
     """Evaluation for target encoder by source classifier on target dataset."""
 
-    # set eval state for Dropout and BN layers
-    src_encoder.eval()
-    tgt_encoder.eval()
-    classifier.eval()
+    for coeff in range(100, 200, 4):
 
-    # init loss and accuracy
-    loss = 0.0
-    acc = 0.0
+        # set eval state for Dropout and BN layers
+        src_encoder.eval()
+        tgt_encoder.eval()
+        classifier.eval()
 
-    # set loss function
-    criterion = nn.CrossEntropyLoss()
+        # init loss and accuracy
+        loss = 0.0
+        acc = 0.0
 
-    # evaluate network
-    for (images, labels) in data_loader:
-        images = make_variable(images, volatile=True)
-        labels = make_variable(labels).squeeze_()
+        # set loss function
+        criterion = nn.CrossEntropyLoss()
 
-        torch.no_grad()
+        # evaluate network
+        for (images, labels) in data_loader:
+            images = make_variable(images, volatile=True)
+            labels = make_variable(labels).squeeze_()
 
-        dists_src = src_detector(images).squeeze_()
-        dists_tgt = tgt_detector(images).squeeze_()
+            torch.no_grad()
+
+            dists_src = src_detector(images).squeeze_()
+            dists_tgt = tgt_detector(images).squeeze_()
 
 
-        src_or_tgt = []
+            src_or_tgt = []
 
-        for dist_src, dist_tgt in zip(dists_src, dists_tgt):
-            dist_src = torch.max(dist_src.squeeze())
-            dist_tgt = torch.max(dist_tgt.squeeze()) * 110
+            for dist_src, dist_tgt in zip(dists_src, dists_tgt):
+                dist_src = torch.max(dist_src.squeeze())
+                dist_tgt = torch.max(dist_tgt.squeeze()) * coeff
 
-            print((dist_src, dist_tgt))
+                #print((dist_src, dist_tgt))
 
-            if dist_src > dist_tgt:
-                src_or_tgt.append(0)
-            else:
-                src_or_tgt.append(1)
+                if dist_src > dist_tgt:
+                    src_or_tgt.append(0)
+                else:
+                    src_or_tgt.append(1)
 
-        preds_src_encoder = classifier(src_encoder(images))
-        preds_tgt_encoder = classifier(tgt_encoder(images))
+            preds_src_encoder = classifier(src_encoder(images))
+            preds_tgt_encoder = classifier(tgt_encoder(images))
 
-        preds = []
+            preds = []
 
-        for origin, pred_src_encoder, pred_tgt_encoder in zip (src_or_tgt, \
-                                    preds_src_encoder, preds_tgt_encoder):
-            pred_src_encoder = pred_src_encoder.cpu().detach().numpy()
-            pred_tgt_encoder = pred_tgt_encoder.cpu().detach().numpy()
-            if origin == 0:
-                preds.append(pred_src_encoder)
-            else:
-                preds.append(pred_tgt_encoder)
+            for origin, pred_src_encoder, pred_tgt_encoder in zip (src_or_tgt, \
+                                        preds_src_encoder, preds_tgt_encoder):
+                pred_src_encoder = pred_src_encoder.cpu().detach().numpy()
+                pred_tgt_encoder = pred_tgt_encoder.cpu().detach().numpy()
+                if origin == 0:
+                    preds.append(pred_src_encoder)
+                else:
+                    preds.append(pred_tgt_encoder)
 
-        preds = torch.Tensor(np.asarray(preds)).cuda()
+            preds = torch.Tensor(np.asarray(preds)).cuda()
 
-        loss += criterion(preds, labels).data
+            loss += criterion(preds, labels).data
 
-        pred_cls = preds.data.max(1)[1]
-        acc += pred_cls.eq(labels.data).cpu().sum()
+            pred_cls = preds.data.max(1)[1]
+            acc += pred_cls.eq(labels.data).cpu().sum()
 
-    loss /= len(data_loader)
-    acc /= len(data_loader.dataset)
+        loss /= len(data_loader)
+        acc /= len(data_loader.dataset)
 
-    print("Avg Loss = {}, Avg Accuracy = {:2%}".format(loss, acc))
+        print("Avg Loss = {}, Avg Accuracy = {:2%}, Coeff = {}".format(loss, acc, coeff))
