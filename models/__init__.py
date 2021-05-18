0f7f189eba41005c0478fcb841e85dbd9be832a5
Ashley Gao <gaoye0718@gmail.com>
2021-05-17 20:03:19 -0400

    wip

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@ from .discriminator import Discriminator
 from .lenet import LeNetClassifier, LeNetEncoder
 from .detector import Detector, DetectorClassifier
 
-__all__ = (LeNetClassifier, LeNetEncoder, Discriminator, Detector, DetectorClassifier
+__all__ = (LeNetClassifier, LeNetEncoder, Discriminator, Detector, DetectorClassifier)
