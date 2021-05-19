8e1fe7cff2879e2d8ed37ac4b82eaff07314139d
Ashley Gao <gaoye0718@gmail.com>
2021-05-18 22:06:15 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -41,7 +41,7 @@ def eval_tgt(src_encoder, tgt_encoder, classifier, data_loader, src_detector, tg
             dist_src = torch.max(dist_src.squeeze())
             dist_tgt = torch.max(dist_tgt.squeeze())
 
-            print((dist_src, dist_tgt))
+            #print((dist_src, dist_tgt))
             if dist_src > 0.05:
                 src_or_tgt.append(0)
             elif dist_tgt > 5:
