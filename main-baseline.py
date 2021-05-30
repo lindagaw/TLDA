a20c0ee0e539b41e1c3818bceae01b27a757ea95
Ashley Gao <gaoye0718@gmail.com>
2021-05-30 16:04:32 -0400

    finished debugging main-baseline.py

--- main-baseline.py
+++ main-baseline.py
@@ -6,7 +6,7 @@ opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
 
-import params
+import params_baseline as params
 from core import eval_src, eval_tgt, train_src, train_tgt, train_baseline, eval_baseline
 from models import Discriminator, LeNetClassifier, LeNetEncoder, Baseline
 from utils import get_data_loader, init_model, init_random_seed
@@ -32,15 +32,14 @@ if __name__ == '__main__':
     print(">>> Source Baseline <<<")
     print(src_baseline)
 
-    if not os.path.isfile('snapshots//src-baseline-final.pt'):
-        src_baseline = train_baseline(src_baseline, src_data_loader)
+    src_baseline = train_baseline(src_baseline, src_data_loader)
 
     # train target baseline
     print("=== Training baseline for target domain ===")
     print(">>> Target Baseline <<<")
     print(tgt_baseline)
-    if not os.path.isfile('snapshots//tgt-baseline-final.pt'):
-        tgt_baseline = train_baseline(tgt_baseline, tgt_data_loader)
+
+    tgt_baseline = train_baseline(tgt_baseline, tgt_data_loader)
 
     # eval source model on source data
     print("=== Evaluating source baseline for source domain ===")
@@ -54,8 +53,11 @@ if __name__ == '__main__':
     print('==================== TL/DA Magic ====================')
     print('=====================================================')
 
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
     # eval source model on target data
+    print('=====================================================')
     print("=== Evaluating source baseline for target domain ===")
     print("=== This is what happens if no TL/DA is applied  ===")
     print("=== get source model's classification on target  ===")
+    print('=====================================================')
     eval_baseline(src_baseline, tgt_data_loader_eval)
