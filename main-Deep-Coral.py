9a0a647c7d6d901b9aaa8abb762d2bde840ed638
Ashley Gao <gaoye0718@gmail.com>
2021-05-30 16:30:54 -0400

    debugging main-ADDA

--- main-Deep-Coral.py
+++ main-Deep-Coral.py
@@ -6,7 +6,7 @@ opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
 
-import params
+import params_deep_coral as params
 from core import eval_src, eval_tgt, train_src, train_tgt, train_coral, eval_coral, CORAL
 from models import Discriminator, LeNetClassifier, LeNetEncoder, Coral
 from utils import get_data_loader, init_model, init_random_seed
