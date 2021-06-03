aa5a959bef54fd48d1a8122cda846476522ea2ad
Auriel G <gaoye0718@gmail.com>
2021-06-03 14:46:48 -0400

    the folder content in the CS department terminal

--- main-baseline.py
+++ main-baseline.py
@@ -6,7 +6,7 @@ opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
 
-import params_baseline as params
+import params
 from core import eval_src, eval_tgt, train_src, train_tgt, train_baseline, eval_baseline
 from models import Discriminator, LeNetClassifier, LeNetEncoder, Baseline
 from utils import get_data_loader, init_model, init_random_seed
