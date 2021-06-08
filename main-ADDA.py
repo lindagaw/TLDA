37a090852f555f32887d0fb0ddf5d3dc6a6e8eb6
Ashley Gao <gaoye0718@gmail.com>
2021-06-07 20:14:27 -0400

    addressed conflict messages

--- main-ADDA.py
+++ main-ADDA.py
@@ -7,15 +7,7 @@ opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
 
 import params
-<<<<<<< HEAD
-<<<<<<< HEAD
 from core import eval_src_encoder, eval_tgt_encoder, train_src_encoder, train_tgt_encoder, eval_ADDA, train_tgt_classifier
-=======
-from core import eval_src_encoder, eval_tgt_encoder, train_src_encoder, train_tgt_encoder
->>>>>>> parent of 87ba083... WIP
-=======
-from core import eval_src_encoder, eval_tgt_encoder, train_src_encoder, train_tgt_encoder
->>>>>>> parent of 87ba083... WIP
 from models import Discriminator, LeNetClassifier, LeNetEncoder
 from utils import get_data_loader, init_model, init_random_seed
 
@@ -80,31 +72,14 @@ if __name__ == '__main__':
             params.tgt_model_trained):
         tgt_encoder = train_tgt_encoder(src_encoder, tgt_encoder, critic,
                                 src_data_loader, tgt_data_loader)
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     tgt_encoder, tgt_classifier = train_tgt_classifier(
         tgt_encoder, tgt_classifier, tgt_data_loader)
-=======
->>>>>>> parent of 87ba083... WIP
-=======
->>>>>>> parent of 87ba083... WIP
 
     # eval target encoder on test set of target dataset
     print("=== Evaluating classifier for encoded target domain ===")
     print(">>> only source detector <<<")
     eval_tgt_encoder(src_encoder, src_classifier, tgt_data_loader_eval)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     print(">>> source + target encoders <<<")
     eval_ADDA(src_encoder, tgt_encoder, src_classifier, tgt_classifier, critic, tgt_data_loader_eval)
-=======
-    #print(">>> only target detector <<<")
-    print(">>> source and target detectors <<<")
-    eval_tgt_encoder(tgt_encoder, src_classifier, tgt_data_loader_eval)
->>>>>>> parent of 87ba083... WIP
-=======
-    #print(">>> only target detector <<<")
-    print(">>> source and target detectors <<<")
-    eval_tgt_encoder(tgt_encoder, src_classifier, tgt_data_loader_eval)
->>>>>>> parent of 87ba083... WIP
